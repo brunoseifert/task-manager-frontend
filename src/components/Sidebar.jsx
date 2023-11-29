0c89dfa0aeb93dbf3bb82e69afd00b199f52ce5b
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-11-29 16:24:13 -0300

    feat: Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import "./Sidebar.scss";
+import CustomButton from "./Button";
+import logo from "../assets/logo-1.png";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-container">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+
+            <div className="sign-out">
+                <CustomButton>Sair</CustomButton>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
